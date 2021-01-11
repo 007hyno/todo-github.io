409e5fe1d9e06d06992020f8bb8d6f392bccc534
007hypernova <parasrawat05@gmail.com>
2021-01-11 17:15:35 +0530

    basic Todo list #js project

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById('input');
+let btn = document.getElementById('btn');
+let table = document.getElementById('main');
+
+btn.addEventListener('click', function(){
+    var row = document.createElement('tr');
+    var l = document.createElement('li');
+    var todo = document.createElement('td');
+    var time = document.createElement('td');
+    // alert(`This is ${row.innerText}`);
+    var d = new Date();
+    todo.innerText = input.value;
+    time.innerText = d.toLocaleTimeString();
+    if(input.value == ""){
+        alert("Todo is empty !!!");
+    }
+    else{
+        table.appendChild(row);
+        row.appendChild(todo);
+        row.appendChild(time);
+    }
+    input.value = "";
+    row.addEventListener('click', function(){
+        row.style.backgroundColor = "#fafafa";
+        todo.style.textDecoration = "line-through";
+        todo.style.color = "#949494";
+        todo.style.fontStyle = "italic";
+        time.style.textDecoration = "line-through";
+        time.style.color = "#949494";
+        time.style.fontSize = "italic";
+    })
+})
